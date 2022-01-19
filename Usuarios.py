a02e20fe26cb51290cef0b39ca68940f0ce12b38
Mauaqp <peraltilla93@hotmail.com>
2022-01-18 23:52:56 -0500

    Eliminación de usuarios

--- Usuarios.py
+++
@@ -1,8 +0,0 @@
-class Usuarios:
-    lista_usuarios = []
-    def __init__(self, nombre, apellido, edad, dni,):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.edad = edad
-        self.dni = dni
-        Usuarios.lista_usuarios.append(self)
